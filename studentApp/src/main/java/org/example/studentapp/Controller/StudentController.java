30dfc97f51855fcd7638eb92f6c7d49aad9927a4
JulitaJaros <julita.paulina.jaros@gmail.com>
2024-02-15 20:55:47 +0100

    add post mapping method

--- studentApp/src/main/java/org/example/studentapp/Controller/StudentController.java
+++ studentApp/src/main/java/org/example/studentapp/Controller/StudentController.java
@@ -1,12 +1,16 @@
 package org.example.studentapp.Controller;
 
+import jdk.dynalink.linker.LinkerServices;
 import lombok.RequiredArgsConstructor;
 import org.example.studentapp.Model.StudentModel;
 import org.example.studentapp.Service.StudentService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.view.RedirectView;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -14,7 +18,9 @@ public class StudentController {
 
     private final StudentService studentService;
     @GetMapping(value = "/students")
-    public String getStudentsList(){
+    public String getStudentsList(Model model){
+        List<StudentModel> studentModelList = studentService.getStudentList();
+        model.addAttribute("studentModel", studentModelList);
         return "person/personList";
     }
     @GetMapping(value = "/students/add")
--- studentApp/src/main/java/org/example/studentapp/Service/StudentService.java
+++ studentApp/src/main/java/org/example/studentapp/Service/StudentService.java
@@ -5,10 +5,15 @@ import org.example.studentapp.Model.StudentModel;
 import org.example.studentapp.Repository.StudentRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class StudentService {
     private final StudentRepository studentRepository;
+    public List<StudentModel> getStudentList(){
+        return studentRepository.findAll();
+    }
 
     public void addStudent(StudentModel studentModel){
         studentRepository.save(studentModel);
